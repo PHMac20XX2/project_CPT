38ccab30d08974d93243828fc858b2cd5791a6a7
PHMac20XX2 <phmestudos@gmail.com>
2022-11-18 00:03:29 -0300

    Modificação Total do Projeto

--- projectCPT.py
+++ projectCPT.py
@@ -1,38 +1,42 @@
 import getpass
 
-def men_ini(text):
-    text = print (" -----> Maquininha de Pagamentos <----- ")
-    return men_ini  
+def men_inicial(x):
+    print (x)
+men_inicial("     Pagamentos     ")
+print (10 * ('---'))
 
-def entry_card():
-    opcao = int (opcao)
-    
-    opcao = input(''' Tipos de cartão aceito
-                    
- [1] Crédito:
- [2] Débito: 
- 
- Digite o número do tipo de cartão: ''')
+def insert_valor(valor):
+    insert_valor = valor 
+valor = (input("Valor da conta: "))
+print (f"Total da conta:{valor}") 
 
-def puxa_opcao(opcao): 
-    entry_card()
+print (10 * ('---'))
 
-    if opcao == 1:
-        print ("Opção escolhida: Crédito:")
-    elif opcao == 2:
-        print ("Opção escolhida: Débito: ")
-    else:
-        print ("Opção incorreta, escolha entre as existentes")
-        
-''' def senha(senha_bd):
-    senha = int (senha_bd)
-senha_bd = 1234
+def entrada_card(opcao):
+    entrada_card = int (opcao)
 
-def maq_func(opcao, senha_bd):
+opcao = int (input ('''
+               
+     Escolha uma das opções:               
+|-------------||-------------------|
+|[1] Crédito  ||[3]Vale-Refeição   |
+|-------------||-------------------|
+|[2] Débito   ||[4]Vale-Alimentação|
+|-------------||-------------------|
+
+:'''))
+
+print (10 * ("----"))
+
+def senha_bd():
+    senha = int (getpass.getpass(prompt="Digite sua senha: ", stream=None))  
+senha = 1234
     
-    entry_card()
+def opc ():
+    entrada_card()
+  
+if opcao == 1:
+    senha_bd()
     
-    puxa_opcao()
-    
-    senha()
-    senha = (getpass.getpass(prompt="Digite a senha: ", stream=None)) '''
+    if senha == 1234:
+        print ("Transação aceita!")
